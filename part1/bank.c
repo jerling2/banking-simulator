4ce40bc4dd1b0e3ee45336b753e73acd8eb4cb44
Joseph Erlinger <josepherlinger@gmail.com>
2024-06-07 10:23:49 -0700

    Add documentation to part1

--- part1/bank.c
+++ part1/bank.c
@@ -2,16 +2,41 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
 #include "account.h"
 #include "fileio.h"
 #include "parser.h"
 #include "request.h"
 #define _GNU_SOURCE
+
+// Macro for turning on/off debug messages.
+#ifdef DEBUG_ENABLED
+#define DEBUG if (1)
+#else
+#define DEBUG if (0)
+#endif
+
+// Macro for turning on/off info messages.
+#ifdef INFO_ENABLED
+#define INFO if (1)
+#else
+#define INFO if (0)
+#endif
+
+// For notifying invalid usuage.
 #define ERROR "\x1b[1;31mERROR\x1b[0m"
 #define USUAGE "%s usuage %s <filename>\n"
 #define STREAM "%s could not open '%s'. %s.\n"
 
 
+/**
+ * @brief The main thread (Single threaded solution)
+ * 
+ * This function is run by the main thread. The main thread opens the input
+ * file, extracts the accounting information, processes each request, and
+ * then exits. This is the single threaded solution, where every instruction
+ * is completed sequentially.
+ */
 int main (int argc, char *argv[]) 
 {
     FILE *stream;
@@ -31,16 +56,24 @@ int main (int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     /* Extract account data */
+    DEBUG printf("-- Part 1 --\n");
+    DEBUG printf("main (pid=%u) is extracting the accounting information...", getpid());
     GetAccounts(stream, &accountArray, &totalAccounts);
+    DEBUG printf("DONE\n");
     /* Process requests from the input file */
+    DEBUG printf("main is processing requests...");
     while ((request = ReadRequest(stream)) != NULL) {
         CommandInterpreter(accountArray, request, totalAccounts);
         FreeCmd(request);
     }
+    DEBUG printf("DONE\n");
     /* Apply the reward rate to all balances */
+    DEBUG printf("main is adding each account's reward to their balance...");
     ProcessReward(accountArray, totalAccounts);
+    DEBUG printf("DONE\n");
     /* Output data to standard out */
-    PrintBalances(accountArray, totalAccounts);
+    INFO printf("Duck Bank Balances:\n");
+    INFO PrintBalances(accountArray, totalAccounts);
     /* Free resources */
     FreeAccountArray(accountArray, totalAccounts);
     fclose(stream);
